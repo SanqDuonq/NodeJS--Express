8cf6435a678f54082e8c2a9c6a5539557d6160f2
SanqDuonq <namsang0902s@gmail.com>
2024-11-28 10:40:11 +0700

    Connect mongoDB

---
+++ helpers/connect-mongo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const connect = mongoose.createConnection('mongodb://localhost:27017/test')
+connect.on('connected',function() {
+    console.log(`MongoDB::: connected::: ${this.name}`)
+})
+
+connect.on('disconnected',function() {
+    console.log(`MongoDB::: disconnected::: ${this.name}`)
+})
+
+connect.on('error', function(err) {
+    console.log(`MongoDB::: error::: ${JSON.stringify(err)}`)
+})
+
+process.on('SIGINT', async () => {
+    await connect.close();
+    process.exit(0);
+})
+
+module.exports = connect;
--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-import express from 'express'
-
+const express = require('express') 
+const dotenv = require('dotenv')
+const createError = require('http-errors')
+const userRoute = require('./routes/user.route')
+require('./helpers/connect-mongo')
+dotenv.config();
 const app = express();
 
 
+app.get('/',(req,res,next) => {
+    res.send('Home Page')
+})
+
+app.use('/api/user',userRoute)
+
+app.use((req,res,next) => {
+   next(createError.NotFound('This router does not exist'))
+})
+ 
+app.use((err,req,res,next) => {
+    res.json({
+        status: err.status,
+        message: err.message
+    })
+})
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
