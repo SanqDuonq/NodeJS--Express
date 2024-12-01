4eb1109bf3abc60a2916565886f73fccc70c1290
SanqDuonq <namsang0902s@gmail.com>
2024-12-01 07:20:08 +0700

    Hash password

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const schema = mongoose.Schema();
-
+const bcrypt = require('bcrypt')
 const userSchema = mongoose.Schema({
     username: {
         type: String,
@@ -10,6 +10,18 @@ const userSchema = mongoose.Schema({
     password: {
         type:String,
         required: true
+    }
+})
+
+userSchema.pre('save', async function (next) {
+    try {
+        console.log('Called before save', this.username,this.password)
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(this.password,salt)
+        this.password = hashPassword
+        next();
+    } catch (error) {
+        next(error)
     }
 })
 
