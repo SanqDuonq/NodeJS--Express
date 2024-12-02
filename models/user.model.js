d8974bdf58e08a6163df049c9d1a26907f807c52
SanqDuonq <namsang0902s@gmail.com>
2024-12-02 14:17:03 +0700

    Login Feature

--- models/user.model.js
+++ models/user.model.js
@@ -25,4 +25,12 @@ userSchema.pre('save', async function (next) {
     }
 })
 
+userSchema.methods.isCheckPassword = async function (password) {
+    try {
+        return await bcrypt.compare(password,this.password)
+    } catch (error) {
+        
+    }
+}
+
 module.exports = mongoose.model('user',userSchema)
